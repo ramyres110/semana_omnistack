3daf542fa3402b69578de32794b71659f7747181
Ramyres Aquino <ramyres90@gmail.com>
2019-06-12 22:10:21 -0300

    Concluido backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
 
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
-mongoose.connect('mongodb+srv://dbuser:dbUserPassword@cluster0-abpto.mongodb.net/test?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://dbuser:dbUserPassword@cluster0-abpto.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true
 });
 
+app.use((req, res, next) => {
+    req.io = io;
+    next();
+})
+
+app.use(cors());
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
+
 app.use(require('./routes'));
 
-app.listen(3000);
+server.listen(3000);
